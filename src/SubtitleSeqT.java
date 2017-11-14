aa2da55a9941591463b17a23c03bd7e135225380
nasser90 <naaasss1@gmail.com>
2017-11-14 21:31:47 +0300

    added convertToMS() and Some Changes
    Added:
    convertToMS(Time t)
    
    Changes:
    getSubtitles()
    getSubtitle(Time time)
    getSubtitles(Time startTime, Time endTime)
    getSubtitles(String str)

--- src/SubtitleSeqT.java
+++ src/SubtitleSeqT.java
@@ -13,245 +13,127 @@ public class SubtitleSeqT implements SubtitleSeq {
 	}
 
 	public List<Subtitle> getSubtitles() {
-		LinkedList<Subtitle> l = new LinkedList<>();
+		List<Subtitle> l1 = new LinkedList<>();
+		List<Subtitle> l2 = new LinkedList<>();
 		
+		if(!Subtitles.empty())
+			Subtitles.findFirst();
+		
+		// copying from the original list to l1 
 		while(!Subtitles.last()){
-			
+			if(!l1.full())
+				l1.insert(Subtitles.retrieve());
+			Subtitles.findNext();
 		}
 		
-		return l;
+		if(!l1.full())
+			l1.insert(Subtitles.retrieve());
+		
+		// Sorting the list from the small stratTime to the biggest one
+		// 
+		while(!l1.empty()){
+			l1.findFirst();
+			
+			Subtitle small = l1.retrieve();
+			int s = 0;
+			int c = 0;
+			
+			while(!l1.last()){
+				if(convertToMS(small.getStartTime()) > convertToMS(l1.retrieve().getStartTime())){
+					small = l1.retrieve();
+					s = c;
+				}
+				c++;
+				l1.findNext();
+			}
+			
+			l1.findFirst();
+			for(int i=0; i<= s; i++){
+				if(!l1.empty() && i == s)
+					l1.remove();
+				l1.findNext();
+			}
+			
+			if(!l2.full())
+				l2.insert(small);
+		}
+		
+		return l2;
 	}
 
-	///////////
+	// 
 	public Subtitle getSubtitle(Time time) {
-		int startT = 0;
-		int endT = 0;
+
 		if(!Subtitles.empty()){
 			Subtitles.findFirst();
 			
 			while(!Subtitles.last()){
-				if(Subtitles.retrieve().getStartTime().getHH() <= time.getHH()){
-					if(Subtitles.retrieve().getStartTime().getHH() == time.getHH()){
-						if(Subtitles.retrieve().getStartTime().getMM() <= time.getMM()){
-							if(Subtitles.retrieve().getStartTime().getMM() == time.getMM()){
-								if(Subtitles.retrieve().getStartTime().getSS() <= time.getSS()){
-									if(Subtitles.retrieve().getStartTime().getSS() == time.getSS()){
-										if(Subtitles.retrieve().getStartTime().getMS() <= time.getMS()){
-											if(Subtitles.retrieve().getStartTime().getMS() == time.getMS()){
-												startT = 1;
-											}
-										}
-									}else
-										startT = 1;
-								}
-							}else
-								startT = 1;
-						}
-					}else
-						startT = 1;
-				}
 				
-				if(Subtitles.retrieve().getEndTime().getHH() >= time.getHH()){
-					if(Subtitles.retrieve().getEndTime().getHH() == time.getHH()){
-						if(Subtitles.retrieve().getEndTime().getMM() >= time.getMM()){
-							if(Subtitles.retrieve().getEndTime().getMM() == time.getMM()){
-								if(Subtitles.retrieve().getEndTime().getSS() >= time.getSS()){
-									if(Subtitles.retrieve().getEndTime().getSS() == time.getSS()){
-										if(Subtitles.retrieve().getEndTime().getMS() >= time.getMS()){
-											if(Subtitles.retrieve().getEndTime().getMS() == time.getMS()){
-												endT = 1;
-											}
-										}
-									}else
-										endT = 1;
-								}
-							}else
-								endT = 1;
-						}
-					}else
-						endT = 1;
-				}
-				if(startT == 1 && endT == 1)
+				if(convertToMS(time) >= convertToMS(Subtitles.retrieve().getStartTime())
+						&& convertToMS(time) <= convertToMS(Subtitles.retrieve().getEndTime()))
 					return Subtitles.retrieve();
+				
 				Subtitles.findNext();
 			}
 			
 			// Begin for the last element
-			if(Subtitles.retrieve().getStartTime().getHH() <= time.getHH()){
-				if(Subtitles.retrieve().getStartTime().getHH() == time.getHH()){
-					if(Subtitles.retrieve().getStartTime().getMM() <= time.getMM()){
-						if(Subtitles.retrieve().getStartTime().getMM() == time.getMM()){
-							if(Subtitles.retrieve().getStartTime().getSS() <= time.getSS()){
-								if(Subtitles.retrieve().getStartTime().getSS() == time.getSS()){
-									if(Subtitles.retrieve().getStartTime().getMS() <= time.getMS()){
-										if(Subtitles.retrieve().getStartTime().getMS() == time.getMS()){
-											startT = 1;
-										}
-									}
-								}else
-									startT = 1;
-							}
-						}else
-							startT = 1;
-					}
-				}else
-					startT = 1;
-			}
-			
-			if(Subtitles.retrieve().getEndTime().getHH() >= time.getHH()){
-				if(Subtitles.retrieve().getEndTime().getHH() == time.getHH()){
-					if(Subtitles.retrieve().getEndTime().getMM() >= time.getMM()){
-						if(Subtitles.retrieve().getEndTime().getMM() == time.getMM()){
-							if(Subtitles.retrieve().getEndTime().getSS() >= time.getSS()){
-								if(Subtitles.retrieve().getEndTime().getSS() == time.getSS()){
-									if(Subtitles.retrieve().getEndTime().getMS() >= time.getMS()){
-										if(Subtitles.retrieve().getEndTime().getMS() == time.getMS()){
-											endT = 1;
-										}
-									}
-								}else
-									endT = 1;
-							}
-						}else
-							endT = 1;
-					}
-				}else
-					endT = 1;
-			}
+			if(convertToMS(time) >= convertToMS(Subtitles.retrieve().getStartTime())
+					&& convertToMS(time) <= convertToMS(Subtitles.retrieve().getEndTime()))
+				return Subtitles.retrieve();
 			// End of last element
 		}
 		
-		if(startT == 1 && endT == 1)
-			return Subtitles.retrieve();
 		return null;
 	}
 
 	//////////////////
 	public List<Subtitle> getSubtitles(Time startTime, Time endTime) {
-		LinkedList<Subtitle> l = new LinkedList<>();
-		int startT = 0;
-		int endT = 0;
-		if(!Subtitles.empty()){
-			Subtitles.findFirst();
+		List<Subtitle> sl = getSubtitles();
+		List<Subtitle> r = new LinkedList<>();
+
+		
+		if(!sl.empty()){
+			sl.findFirst();
 			
-			while(!Subtitles.last()){
-				startT = 0;
-				endT = 0;
-				if(Subtitles.retrieve().getStartTime().getHH() >= startTime.getHH()){
-					if(Subtitles.retrieve().getStartTime().getHH() == startTime.getHH()){
-						if(Subtitles.retrieve().getStartTime().getMM() >= startTime.getMM()){
-							if(Subtitles.retrieve().getStartTime().getMM() == startTime.getMM()){
-								if(Subtitles.retrieve().getStartTime().getSS() >= startTime.getSS()){
-									if(Subtitles.retrieve().getStartTime().getSS() == startTime.getSS()){
-										if(Subtitles.retrieve().getStartTime().getMS() >= startTime.getMS()){
-											if(Subtitles.retrieve().getStartTime().getMS() == startTime.getMS()){
-												startT = 1;
-											}
-										}
-									}else
-										startT = 1;
-								}
-							}else
-								startT = 1;
-						}
-					}else
-						startT = 1;
-				}
+			while(!sl.last()){
 				
-				if(Subtitles.retrieve().getStartTime().getHH() <= endTime.getHH()){
-					if(Subtitles.retrieve().getStartTime().getHH() == endTime.getHH()){
-						if(Subtitles.retrieve().getStartTime().getMM() <= endTime.getMM()){
-							if(Subtitles.retrieve().getStartTime().getMM() == endTime.getMM()){
-								if(Subtitles.retrieve().getStartTime().getSS() <= endTime.getSS()){
-									if(Subtitles.retrieve().getStartTime().getSS() == endTime.getSS()){
-										if(Subtitles.retrieve().getStartTime().getMS() <= endTime.getMS()){
-											if(Subtitles.retrieve().getStartTime().getMS() == endTime.getMS()){
-												startT = 1;
-											}
-										}
-									}else
-										startT = 1;
-								}
-							}else
-								startT = 1;
-						}
-					}else
-						startT = 1;
-				}
-				if(startT == 1 && endT == 1 &&!l.full())
-					l.insert(Subtitles.retrieve());
+				if(!r.full())
+					if(convertToMS(startTime) <= convertToMS(sl.retrieve().getStartTime())
+					&& convertToMS(endTime) >= convertToMS(sl.retrieve().getEndTime()))
+						r.insert(Subtitles.retrieve());
 				
 				Subtitles.findNext();
 			}
 			// Begin for the last element
-			startT = 0;
-			endT = 0;
-			if(Subtitles.retrieve().getStartTime().getHH() >= startTime.getHH()){
-				if(Subtitles.retrieve().getStartTime().getHH() == startTime.getHH()){
-					if(Subtitles.retrieve().getStartTime().getMM() >= startTime.getMM()){
-						if(Subtitles.retrieve().getStartTime().getMM() == startTime.getMM()){
-							if(Subtitles.retrieve().getStartTime().getSS() >= startTime.getSS()){
-								if(Subtitles.retrieve().getStartTime().getSS() == startTime.getSS()){
-									if(Subtitles.retrieve().getStartTime().getMS() >= startTime.getMS()){
-										if(Subtitles.retrieve().getStartTime().getMS() == startTime.getMS()){
-											startT = 1;
-										}
-									}
-								}else
-									startT = 1;
-							}
-						}else
-							startT = 1;
-					}
-				}else
-					startT = 1;
-			}
 			
-			if(Subtitles.retrieve().getStartTime().getHH() <= endTime.getHH()){
-				if(Subtitles.retrieve().getStartTime().getHH() == endTime.getHH()){
-					if(Subtitles.retrieve().getStartTime().getMM() <= endTime.getMM()){
-						if(Subtitles.retrieve().getStartTime().getMM() == endTime.getMM()){
-							if(Subtitles.retrieve().getStartTime().getSS() <= endTime.getSS()){
-								if(Subtitles.retrieve().getStartTime().getSS() == endTime.getSS()){
-									if(Subtitles.retrieve().getStartTime().getMS() <= endTime.getMS()){
-										if(Subtitles.retrieve().getStartTime().getMS() == endTime.getMS()){
-											startT = 1;
-										}
-									}
-								}else
-									startT = 1;
-							}
-						}else
-							startT = 1;
-					}
-				}else
-					startT = 1;
-			}
-			if(startT == 1 && endT == 1 &&!l.full())
-				l.insert(Subtitles.retrieve());
+			if(!r.full())
+				if(convertToMS(startTime) <= convertToMS(sl.retrieve().getStartTime())
+				&& convertToMS(endTime) >= convertToMS(sl.retrieve().getEndTime()))
+					r.insert(Subtitles.retrieve());
 			// End of last element
 		}
 		
-		return l;
+		return r;
 	}
 
 	
 	public List<Subtitle> getSubtitles(String str) {
-		LinkedList<Subtitle> l = new LinkedList<>();
+		List<Subtitle> sl = getSubtitles();
+		List<Subtitle> l = new LinkedList<>();
 		
-		if(!Subtitles.empty()){
-			Subtitles.findFirst();
+		if(!sl.empty()){
+			sl.findFirst();
 			
-			while(!Subtitles.last()){
-				if(Subtitles.retrieve().getText().contains(str))
+			while(!sl.last()){
+				if(sl.retrieve().getText().contains(str))
 					if(!l.full())
-						l.insert(Subtitles.retrieve());
-				Subtitles.findNext();
+						l.insert(sl.retrieve());
+				sl.findNext();
 			}
 			
-			if(Subtitles.retrieve().getText().contains(str))
+			if(sl.retrieve().getText().contains(str))
 				if(!l.full())
-					l.insert(Subtitles.retrieve());
+					l.insert(sl.retrieve());
 		}
 		
 		
@@ -300,8 +182,16 @@ public class SubtitleSeqT implements SubtitleSeq {
 
 	
 	public void cut(Time startTime, Time endTime) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
+	
+	private static int convertToMS(Time t){
+		
+		return  (t.getHH() * 60 * 60 * 1000) +
+				(t.getMM() * 60 * 1000) +
+				(t.getSS() * 1000) +
+				t.getMS();
+	}
 }
